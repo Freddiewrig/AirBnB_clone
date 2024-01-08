70662df3a4650ac5cd5fc972ca6811cd765f7423
Freddiewrig <thecollegeace1@gmail.com>
2024-01-08 16:04:31 +0300

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,14 @@ from uuid import uuid4
 from datetime import datetime
 """Define class BaseModel"""
 
+
 class BaseModel:
     """Init basemodel"""
     def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+  
     def save(self):
         """Update the update_at with the curent time"""
         self.updated_at = datetime.now()
@@ -21,7 +22,8 @@ class BaseModel:
         model_dict["created_at"] = self.created_at.isoformat()
         model_dict["updated_at"] = self.updated_at.isoformat()
         return model_dict
-    
+
     def __str__(self):
         """prints [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
