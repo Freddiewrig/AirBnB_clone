be0e36d3f4b23078108514d779beda6aa47eb758
Freddiewrig <thecollegeace1@gmail.com>
2024-01-10 19:37:18 +0300

    console

--- console.py
+++ console.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """module for HBNBCommand(cmd.Cmd)"""
 import cmd
+import models
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """prompt for comand interpretor"""
     
     prompt = 'hbnb '
+    __classes = {
+        'BaseModel',
+        'user'
+    }
 
     def do_quit(self, arg):
         """to exit the program"""
@@ -20,5 +27,92 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """shouldn’t execute anything"""
         pass
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        class_name= arg.strip()
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = HBNBCommand.__classes[class_name]()
+            storage.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """ Prints the string representation of an instance based on the class name and id"""
+        args = arg.split()
+        class_name = args[0].strip()
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            instance_id = args[1].strip()
+            key = "{}.{}".format(class_name, instance_id)
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """ Deletes an instance based on the class name and id"""
+        args = arg.split()
+        class_name = args[0].strip()
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            instance_id = args[1].strip()
+            key = "{}.{}".format(class_name, instance_id)
+            all_instances = storage.all
+            if key in all_instances:
+                del all_instances[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name"""
+        args = arg.split()
+        if args and args[0] not in HBNBCommand:
+            print("** class doesn't exist **")
+        else:
+            obj_list = []
+            for obj in storage.all().values():
+                if args and args[0] == obj.__class__.__name__:
+                    obj_list.append(obj.__str__())
+                elif not args:
+                    obj_list.append(obj.__str__())
+            print(obj_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split()
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return False
+        if len(args) == 1:
+            print("** instance id missing **")
+        if "{}.{}".format(args[0], args[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(args) == 2:
+            print("** attribute name missing **")
+        if len(args) == 3:
+            try:
+                type(eval(args[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
